7aa714efc5b41081d0c557b3a8c9111cb22f494e
bobocg21 <gabriel.boboc@globalorange.nl>
2019-07-31 23:44:10 +0100

    Add the round up amount for a better response

--- src/main/java/uk/starling/techchallenge/model/RoundUpResponse.java
+++ src/main/java/uk/starling/techchallenge/model/RoundUpResponse.java
@@ -1,11 +1,16 @@
 package uk.starling.techchallenge.model;
 
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 
 public class RoundUpResponse extends StarlingRoundUpResponse {
     //The specific account id against which a round up operation was performed.
     @NotNull
     private String accountId;
+
+    //The round up amount saved in the saving goal
+    @NotNull
+    private BigDecimal roundUpAmount;
 
     public String getAccountId() {
         return accountId;
@@ -13,5 +18,13 @@ public class RoundUpResponse extends StarlingRoundUpResponse {
 
     public void setAccountId(String accountId) {
         this.accountId = accountId;
+    }
+
+    public BigDecimal getRoundUpAmount() {
+        return roundUpAmount;
+    }
+
+    public void setRoundUpAmount(BigDecimal roundUpAmount) {
+        this.roundUpAmount = roundUpAmount;
     }
 }
